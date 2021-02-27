6f7b5230849734ed887232ebcb04564c53755aab
ligwol <yulia-aur@mail.ru>
2021-02-28 01:25:42 +0300

    header 320/1140 adaptive

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -2,9 +2,11 @@
 
 var burger = document.getElementById('burger-menu');
 var menu = document.getElementById('main-menu');
+var header = document.getElementById('header-nav');
 
 var toggleMenu = function toggleMenu() {
   menu.classList.toggle('open');
+  header.classList.toggle('open');
 };
 
 burger.addEventListener('click', toggleMenu);
