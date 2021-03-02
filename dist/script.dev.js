81a65d483801d3f9b6d26b6238e5140829875090
ligwol <yulia-aur@mail.ru>
2021-03-02 22:52:32 +0300

    added image slider

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -10,3 +10,34 @@ var toggleMenu = function toggleMenu() {
 };
 
 burger.addEventListener('click', toggleMenu);
+/******Images Slide*******/
+
+var img1 = document.getElementById('mb-img1');
+var img2 = document.getElementById('mb-img2');
+var img3 = document.getElementById('mb-img3');
+var imArr = document.querySelectorAll('.img');
+
+var img1Slide = function img1Slide() {
+  imArr.forEach(function (ims) {
+    ims.classList.remove('center');
+  });
+  img1.classList.toggle('center');
+};
+
+var img2Slide = function img2Slide() {
+  imArr.forEach(function (ims) {
+    ims.classList.remove('center');
+  });
+  img2.classList.toggle('center');
+};
+
+var img3Slide = function img3Slide() {
+  imArr.forEach(function (ims) {
+    ims.classList.remove('center');
+  });
+  img3.classList.toggle('center');
+};
+
+img1.addEventListener('click', img1Slide);
+img2.addEventListener('click', img2Slide);
+img3.addEventListener('click', img3Slide);
--- script.js
+++ script.js
@@ -7,3 +7,33 @@ const toggleMenu = () => {
     header.classList.toggle('open');
 }
 burger.addEventListener('click', toggleMenu);
+
+/******Images Slide*******/
+const img1 = document.getElementById('mb-img1');
+const img2 = document.getElementById('mb-img2');
+const img3 = document.getElementById('mb-img3');
+const imArr = document.querySelectorAll('.img');
+
+const img1Slide = () => {
+    imArr.forEach(ims => {
+        ims.classList.remove('center');
+    });
+    img1.classList.toggle('center');
+}
+const img2Slide = () => {
+    imArr.forEach(ims => {
+        ims.classList.remove('center');
+    });
+    img2.classList.toggle('center');
+}
+const img3Slide = () => {
+    imArr.forEach(ims => {
+        ims.classList.remove('center');
+    });
+    img3.classList.toggle('center');
+}
+
+img1.addEventListener('click', img1Slide);
+img2.addEventListener('click', img2Slide);
+img3.addEventListener('click', img3Slide);
+
